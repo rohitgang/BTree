ac0458eef7403ecf7c8a3289a40dd02bcc8888be
Ben Peterson <blpeter@gmail.com>
2018-05-02 15:21:39 -0600

    Delete DNA.java

--- DNA.java
+++
@@ -1,9 +0,0 @@
-package BTree;
-
-public class DNA 
-{
-	public static final int A = 0; //00 
-	public static final int T = 3; //11
-	public static final int C = 1; //01
-	public static final int G = 2; //10
-}
