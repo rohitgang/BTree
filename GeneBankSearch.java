7852755a46c5da39247ee1210b7fe585d309b34c
rohitgang <43307547+rohitgang@users.noreply.github.com>
2019-05-09 00:24:03 -0600

    Update GeneBankSearch.java

--- GeneBankSearch.java
+++ GeneBankSearch.java
@@ -1,76 +1,208 @@
-package BTree;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.Scanner;
 
+/*
+ * driver class to search for a key in an object
+ * 
+ * USAGE:
+ * 								1				2			3				4				5
+ * java GeneBankSearch <0/1(no/with Cache)> <btree file> <query file> [<cache size>] [<debug level>]
+ */
 public class GeneBankSearch {
+
+	static boolean isCacheUsed = false ;
+	static int cacheSize;
+	static BTree tree ;
+	static BTreeNode root ;
+	static int degree;
+	static int nodeOffset;
 	
-	static int cacheCapacity, metaSeqLength, debugLevel, cacheSize;
-	static String btreeFileName, queryFileName, metadataFileName;
-	static long searchedKey;
-	static Cache cache;
-	static BTree bt;
-
-	public static void main(String args[]) throws URISyntaxException {
-
-		parseArgs(args);
+	private static RandomAccessFile BTFile;
+	private static FileChannel dataChannel;
+	
+	
+	
+	public static void main(String[] args) throws IOException {
 		
-		try {		
-			BTree bt = new BTree(new File(btreeFileName), new File(metadataFileName), cache);
-
-			System.out.println("Btree File:" + btreeFileName);
-			System.out.println("Metadata File:" + metadataFileName);
-			System.out.println("Query File:" + queryFileName);
-
-			Scanner queryScanner = new Scanner(new File(queryFileName));
-			String curLine = "";
-			do{				
-				curLine = queryScanner.nextLine();
-				long k = bt.sequenceToLong(curLine);
-				BTreeNode searchKey = bt.search(bt.root, k);
-				if(searchKey == null) return;
-				for(int i = 0; i < searchKey.keys.length; i++){
-					if(searchKey.keys[i].key == k)
-						System.out.println(bt.longToSequence(k, metaSeqLength) + " " + searchKey.keys[i].freq);
-				}				
-			}while(queryScanner.hasNextLine());			
-
-		}catch(FileNotFoundException e){
-			e.printStackTrace();
-		}catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	//GeneBankSearch <0/1 with/without Cache> <btree file> <query file> <Cache Size> [<debug level>]
-	public static void parseArgs(String args[]){
-		if(args.length > 5 || args.length < 3 || !args[4].equals("0"))	
+		
+		//checks to make sure args array is in the possible range 3-5
+		if (args.length < 3 || args.length > 5) {
 			printUsage();
+		}
 		
-		if(args.length == 5)
-			debugLevel = Integer.parseInt(args[4]);
-
-		if(args[0].equals("1")){
+		//args 1
+//		if(args[0].equals("1")) {
+//			isCacheUsed = true;
+//		} else if (args[0].equals("0") || args[0].equals("1")){
+//			printUsage();
+//		}
+		int dotCount = 0;
+		int seqLength = 0;
+		
+		for(int i= args[1].length()-1 ; i >= 0; i--) {
+			char ch = args[1].charAt(i) ;
+			if(ch == '.') {
+				dotCount += 1 ;
+				continue;
+			}
+			if(dotCount == 0)
+				degree =  Character.getNumericValue(ch) ;
+			if(dotCount == 1)
+				seqLength = Character.getNumericValue(ch) ;
+			if(dotCount == 2)
+				break ;
+		}
+		
+		nodeOffset = (32 * degree) - 3;
+		// Get BTree metadata, open file, start stream---------------------------------------------------
+		BTFile = new RandomAccessFile(new File(args[1]), "r"); //BTree File
+		dataChannel = BTFile.getChannel();
+		
+		
+		BTFile.seek(12);
+		int rootAddress = BTFile.readInt();
+		
+		root = readNode(rootAddress);
+		
+		
+		//-------------------------------------------------------------------------------------
+		File queryFile = new File(args[2]); //Query File
+		
+		if (isCacheUsed=true && args.length >= 4) {
 			cacheSize = Integer.parseInt(args[3]);
-			cache = new Cache(cacheCapacity);
-		}else {
-			cache = null;
+		}	
+		
+		//System.out.println("degree: "+degree);
+		searchProcedure(queryFile);
+	}
+	
+	
+	public static BTreeNode readNode(int address) {
+		BTreeNode storedNode = new BTreeNode(degree);
+		try {
+			ByteBuffer buffer = ByteBuffer.allocate(nodeOffset + 100); // CHANGE TO MAX
+
+			dataChannel.read(buffer, address);
+			buffer.flip();
+			storedNode.recover(buffer);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return storedNode;
+	}
+	
+	public static TreeObject search(BTreeNode node, long key) {
+		int i = 0;
+		while (i < node.getObjectCount() && key > node.objectArray[i].getKey()) {
+			i = i + 1;
 		}
 
-		btreeFileName = args[1];
-		queryFileName = args[2];
-		metadataFileName = btreeFileName.replace("data", "metadata");
-		metaSeqLength = Integer.parseInt(metadataFileName.split("\\.")[4]);
+		if (i < node.getObjectCount() && key == node.objectArray[i].getKey()) {
+			return node.objectArray[i]; // key matches the object's key, so search successful and return
+		} else {
+			if (node.isLeaf()) {
+				return null;
+			} else {
+				BTreeNode childNode = readNode(node.childPtrs[i]);
+				return search(childNode, key);
+			}
+		}
 	}
+	
 
-	private static void printUsage(){
-		System.err.println("Usage: Java GeneBankSearch "
-				+ "<0/1(no/with Cache)> <Btree File>"
-				+ " <Query File> <Cache Size> [<Debug level>]");
-		System.exit(1);
+	
+	public static void searchProcedure(File query) throws FileNotFoundException {
+		Scanner scan = new Scanner(query) ;
+		while(scan.hasNextLine()) {
+			String geneSeq = scan.nextLine();
+			long key = dnaToBinary(geneSeq);
+			TreeObject object = search(root, key);
+			if(object != null)
+				System.out.println(object.toString());
+		}
+		scan.close();
 	}
+	
+	
 
+	public static long dnaToBinary(String dnaBases) {
+		// Convert string to char Array.
+		// Initialize string-builder with 1(to prevent loss of leading zeroes).
+		// For each character in array, append corresponding binary value to string.
+		// Convert string-builder to a long and return it.
+		char dnaChar[] = dnaBases.toCharArray();
+		StringBuilder temp = new StringBuilder("1"); // Prevents loss of leading zeroes for later conversion to letter
+														// bases.
+		for (char gene : dnaChar) {
+			switch (gene) {
+			case 'a':
+			case 'A':
+				temp.append("00");
+				break;
+			case 'c':
+			case 'C':
+				temp.append("01");
+				break;
+			case 'g':
+			case 'G':
+				temp.append("10");
+				break;
+			case 't':
+			case 'T':
+				temp.append("11");
+				break;
+			}
+		}
+		long binaryDNA = Long.parseLong(temp.toString(), 2); // Converts the string to a long (w/ radix 2 for binary)
+		return binaryDNA;
+	}
+	
+	/**
+	 * Converts a long (in binary form) to a String containing its corresponding
+	 * gene bases. (brandon)
+	 * 
+	 * @param long binaryDNA
+	 * @return String geneString
+	 */
+	public static String binaryToDNA(long binaryDNA) {
+		// Convert given long to its string representation. Initialize geneString.
+		// Skip the leading 1 in the long, compare every two bits against the gene
+		// values, append correct gene base.
+		String num = Long.toBinaryString(binaryDNA);
+		StringBuilder geneString = new StringBuilder();
+		for (int i = 1; i < num.length(); i += 2) { // Skip the first protecting '1' in string, compare each two bits
+			String build = "" + num.charAt(i) + num.charAt(i + 1); // Forms string with two bits, you need the ""
+			switch (build) {
+			case "00":
+				geneString.append('A');
+				break;
+			case "01":
+				geneString.append('C');
+				break;
+			case "10":
+				geneString.append('G');
+				break;
+			case "11":
+				geneString.append('T');
+				break;
+			}
+		}
+		return geneString.toString();
+	}
+	// prints the usage of the program
+	public static void printUsage () {
+		System.err.println("java GeneBankSearch <0/1(no/with Cache)> <btree file> <query file> [<cache size>] [<debug level>]");
+		System.err.println("cache: "		+ "iscacheused"		+ "\n");
+		System.err.println("btree file: "	+ "SOMETHING.gbk" 	+ "\n");
+		System.err.println("query file: "	+ "query_result" 	+ "\n");
+		System.err.println("cache size: " 	+ "cacheSIZE" 		+ "\n");
+		System.err.println("debug level: " 	+ "debugLevel" 		+ "\n");
+		System.exit(1);	//some issue occurred for usage to be printed, likely incorrect argument values
+	}
 }
